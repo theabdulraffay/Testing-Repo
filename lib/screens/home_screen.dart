6d9e1ded847d8d4ee6fb0b16533909297ad65dc9
theabdulraffay <abdulraffay2494@gmail.com>
2025-03-25 19:05:57 +0500

    feat: Enhance Home and Add Post screens with Firebase integration

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,9 @@
+import 'package:dummy_project/screens/login_screen.dart';
 import 'package:dummy_project/screens/post_screen/add_post.dart';
+import 'package:dummy_project/screens/sign_up_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,10 +14,62 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final ref = FirebaseDatabase.instance.ref('Post');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Home Screen')),
+      appBar: AppBar(
+        title: Center(child: Text('Home Screen')),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (builder) => SignUpScreen()),
+              );
+            },
+            icon: Icon(Icons.logout),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          // Expanded(
+          //   child: FirebaseAnimatedList(
+          //       query: ref,
+          //       itemBuilder: (context, snapshot, animation, index) {
+          //         return ListTile(
+          //           title: Text(snapshot.child('title').value.toString()),
+          //           subtitle: Text('This is post ${index + 1}'),
+          //         );
+          //       }),
+          // ),
+          Expanded(
+              child: StreamBuilder(
+                  stream: ref.onValue,
+                  builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
+                    if (snapshot.hasData) {
+                      Map<dynamic, dynamic> map =
+                          snapshot.data!.snapshot.value as dynamic;
+                      List<dynamic> list = map.values.toList();
+
+                      return ListView.builder(
+                        itemCount: snapshot.data!.snapshot.children.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(list[index]['title']),
+                            subtitle: Text(list[index]['time']),
+                            // subtitle: Text('This is post ${index + 1}'),
+                          );
+                        },
+                      );
+                    } else {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                  }))
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
--- lib/screens/post_screen/add_post.dart
+++ lib/screens/post_screen/add_post.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class AddPost extends StatefulWidget {
@@ -8,6 +9,8 @@ class AddPost extends StatefulWidget {
 }
 
 class _AddPostState extends State<AddPost> {
+  final databaseRef = FirebaseDatabase.instance.ref('Post');
+  final postController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,16 +19,29 @@ class _AddPostState extends State<AddPost> {
         spacing: 30,
         children: [
           TextFormField(
+            controller: postController,
             decoration: InputDecoration(
               hintText: 'What is in your mind?',
               border: OutlineInputBorder(),
             ),
             maxLines: 4,
           ),
-
           ElevatedButton(
-            onPressed: () {
+            onPressed: () async {
               // Add post to firebase
+
+              try {
+                await databaseRef
+                    .child(DateTime.now().microsecondsSinceEpoch.toString())
+                    .set({
+                  'title': postController.text,
+                  'time': DateTime.now().toIso8601String(),
+                });
+                // postController.clear();
+              } catch (e) {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text(e.toString())));
+              }
             },
             child: Text('Add'),
           ),
